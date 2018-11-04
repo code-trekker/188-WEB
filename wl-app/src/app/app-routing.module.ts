c0421ca3e40446a43bc77bcfc525366090b4b149
leahermac <leahermac21@gmail.com>
2018-11-04 16:14:36 +0800

    workout component

--- wl-app/src/app/app-routing.module.ts
+++ wl-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { WorkoutComponent } from './workout/workout.component';
 
 const routes: Routes = [
-	{ path: 'profile', component: ProfileComponent }
+	{ path: 'profile', component: ProfileComponent },
+	{ path: 'workout', component: WorkoutComponent }
 ];
 
 @NgModule({
--- wl-app/src/app/app.module.ts
+++ wl-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { WorkoutComponent } from './workout/workout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    WorkoutComponent
   ],
   imports: [
     BrowserModule,
